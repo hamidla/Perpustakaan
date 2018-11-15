8f4fb6dcac47195d35850cfd877ba529968e84d5
hamidla <hamid.laeta@gmail.com>
2018-11-15 21:27:42 +0700

    tugas projek

---
+++ index.php
@@ -0,0 +1,121 @@
+<?php
+        $koneksi = new mysqli("localhost","root","","db_perpustakaan");
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PERPUSTAKAAN CEPAT PINTAR</title>
+	<!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+   <link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">PERPUSTAKAAN CEPAT PINTAR</a> 
+            </div>
+  <div style="color: white;
+padding: 15px 50px 5px 50px;
+float: right;
+font-size: 16px;"> Last access : 30 May 2014 &nbsp; <a href="login.html" class="btn btn-danger square-btn-adjust">Keluar</a> </div>
+        </nav>   
+           <!-- /. NAV TOP  -->
+                <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+				<li class="text-center">
+                    <img src="assets/img/find_user.png" class="user-image img-responsive"/>
+					</li>
+                    <li>
+                        <a  href="index.php"><i class="fa fa-dashboard fa-3x"></i> Dashboard</a>
+                    </li>
+                     <li>
+                        <a  href="?page=anggota"><i class="fa fa-dashboard fa-3x"></i> Data Anggota</a>
+                    </li>
+                    <li>
+                        <a  href="?page=buku"><i class="fa fa-dashboard fa-3x"></i> Data Buku</a>
+                    </li>
+                    <li>
+                        <a  href="?page=transaksi"><i class="fa fa-dashboard fa-3x"></i> Transaksi</a>
+                    </li>
+                </ul>
+               
+            </div>
+            
+        </nav>  
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     
+                     <?php
+
+                     		$page = isset($_GET['page']) ? $_GET['page'] : null;
+                     		$aksi = isset($_GET['aksi']) ? $_GET['aksi'] : null;
+                     		if ($page =="buku"){
+                     			if ($aksi ==""){
+                     				include "page/buku/buku.php";
+                     			}
+                     		}
+                            elseif ($page =="anggota"){
+                                if ($aksi ==""){
+                                    include "page/anggota/anggota.php";
+                                }
+                            }
+                            elseif ($page =="transaksi"){
+                                if ($aksi ==""){
+                                    include "page/transaksi/transaksi.php";
+                                }
+                            }
+
+                     ?>
+                       
+                    </div>
+                </div>
+                 <!-- /. ROW  -->
+                 <hr />
+               
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+      <!-- CUSTOM SCRIPTS -->
+    
+    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
+    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+        <script>
+            $(document).ready(function () {
+                $('#dataTables-example').dataTable();
+            });
+    </script>
+         <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+   
+</body>
+</html>
---
+++ page/transaksi/transaksi.php
@@ -0,0 +1,27 @@
+ <div class="row">
+                <div class="col-md-12">
+                    <!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                                TABEL TRANSAKSI
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>NO</th>
+                                            <th>JUDUL</th>
+                                            <th>PENGARANG</th>
+                                            <th>PENERBIT</th>
+                                            <th>ISBN</th>
+                                            <th>JUMLAH BUKU</th>
+                                            <th>AKSI</th>                                         
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                   		<tr>
+                                   			<td>test</td>>
+                                   		</tr>
+
+                                    </tbody>
